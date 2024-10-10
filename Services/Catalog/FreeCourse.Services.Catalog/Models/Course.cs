42bf99cc6a4f2b823bb434be42b893b8b6606548
Your Name <you@example.com>
2024-10-10 11:27:33 +0400

    CreatedCatalogApi

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string UserId { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+        public Feature Feature { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Feature.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Feature
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
